193ce3bb4bfc4e07ccabdc4a3ab43f2a04330b83
GarusK <garusufa@gmail.com>
2026-08-18 21:25:02 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +8,5 @@ def hello():
     return "Server is running!"
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
